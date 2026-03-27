22695bbdfdf010c82196cdcf2a085526e3a5a74a
yulahong <hongula2000@gmail.com>
2026-03-27 23:45:53 +0900

    [BOJ] 2468_안전영역

--- yulahong/BOJ/1012.py
+++ yulahong/BOJ/1012.py
@@ -1,16 +1,40 @@
-# 단지 문제랑 비슷
-# dfs
-# VISITED배열 사용
-# 좌표 돌면서 ARR 완성
+from collections import deque
+
+def bfs(R, C):
+    queue = deque([(R, C)])
+    visited[R][C] = True
+
+    while queue:
+        r, c = queue.popleft()
+        for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            nr, nc = r + dr, c + dc
+
+            if 0 <= nr < N and 0 <= nc < M:
+                if area[nr][nc] == 1 and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    queue.append((nr, nc))
 
 T = int(input())
-M, N, K = map(int, input().split())
-arr = [[0]*M for _ in range(N)]
 
-for _ in range(K):
-    a, b = map(int, input().split())
+for tc in range(1, T+1):
+    M, N, K = map(int, input().split())
 
-    arr[a][b] = 1
+    area = [[0]*M for _ in range(N)]
 
-print(arr)
+    for _ in range(K):
+        a, b = map(int, input().split())
+        # if 0 <= a < M and 0 <= b < N:
+        area[b][a] = 1
+
+    visited = [[False]*M for _ in range(N)]
+
+    answer = 0
+
+    for r in range(N):
+        for c in range(M):
+            if area[r][c] == 1 and not visited[r][c]:
+                bfs(r, c)
+                answer += 1
+
+    print(answer)
 
